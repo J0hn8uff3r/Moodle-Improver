2c431669f10621bd902ffbb201f74cff9ad381f6
J0hn8uff3r <bufferoverflow@hotmail.es>
2016-10-18 02:29:16 +0200

    Update Moodle_Improver.user.js

--- Moodle_Improver.user.js
+++ Moodle_Improver.user.js
@@ -8,30 +8,29 @@
 // ==/UserScript==
 // TODO
 // Get images from clipboard and paste them directly on message
+function handlePaste(e) {
+    var clipboardData, pastedText;
 
-function handlePaste (e) {
-		var clipboardData, pastedText;
-		
-		e.stopPropagation();
-		e.preventDefault();
-		
-		message_content=document.getElementById("id_message").value;
-		
-		clipboardData = e.clipboardData || window.clipboardData;
-		pastedText = clipboardData.getData('Text');
-	
-	// Check for URL on pasted text and make it clickable
-	if (/\bhttps?:\/\/[\d.a-z-]+\.[a-z]{2,6}(?::\d{1,5})?(?:\/[\d!$'()*+,._a-z-]+){0,9}(?:\/[\d!$'()*+,._a-z-]*)?(?:\?[\d!$&'()*+,.=_a-z-]*)?/i.test(pastedText)) {
-		if (/^https?:\/\/(?:[a-z\-]+\.)+[a-z]{2,6}(?:\/[^\/#?]+)+\.(?:jpe?g|gif|png)$/.test(pastedText)) {//Check if it's an image URL
-			pastedText='<a href="'+pastedText+'" target="_blank"><img src="'+pastedText+'" /></a>';
-			document.getElementById("id_message").value=message_content+pastedText;
-			} else {//Not an image
-				pastedText='<a href="'+pastedText+'">'+pastedText+'</a>';
-				document.getElementById("id_message").value=message_content+pastedText;
-			}
-		} else {
-			document.getElementById("id_message").value=message_content+pastedText;
-		}
+    e.stopPropagation();
+    e.preventDefault();
+
+    message_content = document.getElementById("id_message").value;
+
+    clipboardData = e.clipboardData || window.clipboardData;
+    pastedText = clipboardData.getData('Text');
+
+    // Check for URL on pasted text and make it clickable
+    if (/\bhttps?:\/\/[\d.a-z-]+\.[a-z]{2,6}(?::\d{1,5})?(?:\/[\d!$'()*+,._a-z-]+){0,9}(?:\/[\d!$'()*+,._a-z-]*)?(?:\?[\d!$&'()*+,.=_a-z-]*)?/i.test(pastedText)) {
+        if (/^https?:\/\/(?:[a-z\-]+\.)+[a-z]{2,6}(?:\/[^\/#?]+)+\.(?:jpe?g|gif|png)$/.test(pastedText)) { //Check if it's an image URL
+            pastedText = '<a href="' + pastedText + '" target="_blank"><img src="' + pastedText + '" /></a>';
+            document.getElementById("id_message").value = message_content + pastedText;
+        } else { //Not an image
+            pastedText = '<a href="' + pastedText + '">' + pastedText + '</a>';
+            document.getElementById("id_message").value = message_content + pastedText;
+        }
+    } else {
+        document.getElementById("id_message").value = message_content + pastedText;
+    }
 
 }
 
